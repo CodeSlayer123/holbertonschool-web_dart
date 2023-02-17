8c0e99cd5134001a6358404ef7520b682a567825
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-02-17 19:30:56 +0000

    tasks 1-7

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,10 @@
+void outer(String name, String id){
+  String inner(){
+    final split = name.split(' ');
+    String first = split[0];
+    String last = split[1];
+
+    return 'Hello Agent ${last[0]}.${first} your id is ${id}\n';
+  }
+  print(inner());
+}
---
+++ 0x01-dart-function/5-calculate-area.dart
@@ -0,0 +1,6 @@
+
+double calculateArea(double height, double base){
+  double area = (height * base)/2;
+  double num1 = double.parse((area).toStringAsFixed(2));
+  return num1;
+}
