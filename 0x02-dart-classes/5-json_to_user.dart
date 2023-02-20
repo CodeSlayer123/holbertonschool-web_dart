0125aaf0ca2f01b977d348ed18420f101a4b6e5a
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-02-20 21:14:32 +0000

    added typo for hecker

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -15,6 +15,6 @@ class User {
 
    @override
    String toString() {
-    return 'User(id : $id ,name: $name, age: $age, height: $height)';
+    return 'User(id : $id ,name: $name,  age: $age, height: $height)';
   }
 }
